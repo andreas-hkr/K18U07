a822bbf918edd32797aa2f4c7470b5ebd819c1ed
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-21 18:12:32 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // Låt användaren mata in ett romerskt tal, översätt till vanligt tal och visa på skärmen
+        //  En HashMap som lagrar översättingen enligt:
+        //      I = 1           C = 100
+        //      V = 5           D = 500
+        //      X = 10          M = 1000
+        //      L = 50
+        //
+        // Några tal att testa:
+        // I        = 1
+        // IV       = 4
+        // VII      = 7
+        // XVI      = 16
+        // MMXXIII  = 2023
+
+
+    }
+}
